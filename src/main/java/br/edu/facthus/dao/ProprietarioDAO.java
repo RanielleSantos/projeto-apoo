556a286f17f0f4ebefb4620d90f64d3aaa18ed8a
rani1 <rani1@192.168.0.29>
2021-06-28 18:37:11 -0300

    Tarefa 15 - grupo 4

--- src/main/java/br/edu/facthus/dao/ProprietarioDAO.java
+++ src/main/java/br/edu/facthus/dao/ProprietarioDAO.java
@@ -2,11 +2,16 @@ package br.edu.facthus.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.facthus.exception.CustomException;
 import br.edu.facthus.model.Pessoa;
+
 
 // Tarefa 15: completar...
 public class ProprietarioDAO {
@@ -25,13 +30,53 @@ public class ProprietarioDAO {
 		}
 	}
 	
-	public void insere(Pessoa proprietario) {
-		// completar...
+	public void insere(Pessoa proprietario) 
+	{
+		
+		try {
+			Connection connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(
+					"INSERT INTO pessoas (nome, cpf, telefone) "
+					+ "VALUES (?, ?)");
+			
+			statement.setString(1, proprietario.getNome());
+			statement.setString(2, proprietario.getCpf());
+			statement.setString(2, proprietario.getTelefone());
+			
+			statement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new CustomException("Ocorreu um erro ao inserir .");
+		}
 	}
 	
-	public List<Pessoa> pesquisa() {
-		// completar...
+	public List<Pessoa> pesquisa() 
+	{
+		List<Pessoa> Proprietario = new ArrayList<>();
+		
+		try {
+			Connection connection = getConnection();
+			Statement Statement = connection.createStatement();
+			
+			ResultSet rs = Statement.executeQuery(
+					"SELECT id, nome, cpf, telefone FROM pessoas ORDER BY email");
+			
+			while (rs.next()) 
+			{
+				Pessoa proprietario01 = new Pessoa();
+				proprietario01.setId(rs.getInt("id"));
+				proprietario01.setNome(rs.getString("nome"));
+				proprietario01.setCpf(rs.getString("cpf"));
+				proprietario01.setTelefone(rs.getString("telefone"));
+				
+				Proprietario.add(proprietario01);
+			}
+		
 		return null;
-	}
+	} catch (SQLException e) {
+		e.printStackTrace();
+		throw new CustomException("Ocorreu um erro ao pesquisar .");
+	}		
+}
 
 }
